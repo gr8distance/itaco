349240bb8839ac676c36516c32ab56c140a4c3cb
ug <gr8.distance@gmail.com>
2021-05-10 13:01:05 +0900

    🍷 ex_doc

--- mix.exs
+++ mix.exs
@@ -6,7 +6,13 @@ defmodule Itaco.MixProject do
       app: :itaco,
       version: "1.0.0",
       elixir: "~> 1.11",
+      description: "itaco is a wrapper that assists ecto",
       start_permanent: Mix.env() == :prod,
+      package: [
+        maintainers: ["gr8distance"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/gr8distance/itaco"}
+      ],
       deps: deps()
     ]
   end
@@ -21,8 +27,7 @@ defmodule Itaco.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
